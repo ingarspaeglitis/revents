12bac1baa83855a8f63aa904a98d091de76e1327
Ingars Paeglitis <ingars.paeglitis@yahoo.com>
2020-08-02 13:22:37 +0300

    End of section 1

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/layout/App';
+import './app/layout/styles.css';
+import * as serviceWorker from './serviceWorker';
+
+const rootEl =  document.getElementById('root');
+
+function render() {
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootEl
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', function () {
+    setTimeout(render);
+  })
+}
+
+render();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
