a438dd6447a37d30612216ec71def7301044d251
ingarspaeglitis <ingars@standardsdigital.com>
2023-12-12 00:44:43 +0200

    add new comoponents

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,9 +1,19 @@
+import { Container } from "semantic-ui-react"
+import EventDashboard from "../../features/events/dashboard/EventDashboard"
+import NavBar from "./nav/NavBar"
 
 function App() {
   
 
   return (
-    <h1>Welcome to Revents</h1>   
+    <>
+       <NavBar/>
+       <Container className='main'>
+        <EventDashboard/>
+       </Container>
+       
+    </>
+   
   )
 }
 
